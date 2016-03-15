55ec7376a4590da37e2c0100088fde7ff5ebac99
Bogdan Kobylinsky <92bogdan@gmail.com>
2016-03-15 19:59:10 +0200

    Additional test cases in SortTest

--- src/test/java/com/kobylinsky/algorithms/sorting/SortTest.java
+++ src/test/java/com/kobylinsky/algorithms/sorting/SortTest.java
@@ -1,6 +1,5 @@
 package com.kobylinsky.algorithms.sorting;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -8,6 +7,9 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Random;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * @author bogdankobylinsky
@@ -19,19 +21,33 @@ public class SortTest {
 
     @Parameterized.Parameters(name = "{0}")
     public static Collection<Object[]> arrays() {
-        return Arrays.asList(new Object[][] { { newRandomArray(100) } });
+        return asList(new Object[][] {
+                { new Integer[]{} },
+                { new Integer[]{0} },
+                { new Integer[]{0, 1} },
+                { new Integer[]{0, 0, 0, 0, 0} },
+                { newRandomArray(10) },
+        });
+    }
+
+    public SortTest(Integer[] testArray) {
+        this.testArray = testArray;
     }
 
     @Test
     public void testSelectionSort() {
         // Given
         final Sort selectionSort = new SelectionSort();
+        System.out.println("Initial: " + Arrays.toString(testArray));
         final Integer[] expected = getProperlySortedArray(testArray);
+        System.out.println("Expected: " + Arrays.toString(expected));
 
         // When
         selectionSort.sort(testArray);
+        System.out.println("Actual: " + Arrays.toString(testArray));
 
-        Assert.assertArrayEquals(expected, testArray);
+        // Then
+        assertArrayEquals(expected, testArray);
     }
 
     private static Integer[] newRandomArray(final int size) {
